2c31272e31bf967a46174107b9f16944a0065beb
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-12 01:47:01 +0900

    added nav header home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <Home />
     </div>
-    );
-  }
+  );
+}
 
-  export default App;
+export default App;
