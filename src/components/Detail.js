4cd9167a68e600cb87ab2d68de307ba1e5977f6c
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-12 18:36:42 +0900

    added details page, but switch isnt importing

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Detail = () => {
   return (
-    <div>
-      Detail
-    </div>
+    <Container>
+      <Background>
+        <img src="/images/login-background.jpg" />
+      </Background>
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw - 5px);
+
+  `
+
+  const Background = styled.div`
+  
+  `
