fe307575662a5b0219921baca8c0dccfe0df25d6
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-12 16:58:36 +0900

    added movies section

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -10,16 +10,16 @@ const Viewers = () => {
         <img src="/images/viewers-disney.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-pixar.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-marvel.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-starwars.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-national.png" />
       </Wrap>
     </Container>
   );
@@ -42,19 +42,22 @@ const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-
-
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
   img {
     width: 100%;
     height: 100%;
-    pbject-fit: cover:
+    object-fit: cover:
   }
 
   //&<<<<< concatinates parent and given child
 
   &:hover {
-    
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+
   }
 
   `
