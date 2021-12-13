346097d2d48ba0cafa7e0356d12f031b0672bdac
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-13 18:11:49 +0900

    removed ugly links

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,27 +8,27 @@ function Header() {
     <Nav>
       <Logo src="/images/logo.svg" alt="" />
       <NavMenu>
-        <a href="#">
+        <a>
           <img src="/images/home-icon.svg" alt="" />
           <span> Home </span>
         </a>
-        <a href="#">
+        <a>
           <img src="/images/search-icon.svg" alt="" />
           <span> Search </span>
         </a>
-        <a href="#">
+        <a>
           <img src="/images/watchlist-icon.svg" alt="" />
           <span> Watchlist </span>
         </a>
-        <a href="#">
+        <a>
           <img src="/images/original-icon.svg" alt="" />
           <span> Originals </span>
         </a>
-        <a href="#">
+        <a>
           <img src="/images/movie-icon.svg" alt="" />
           <span> Movies </span>
         </a>
-        <a href="#">
+        <a>
           <img src="/images/series-icon.svg" alt="" />
           <span> Series </span>
         </a>
