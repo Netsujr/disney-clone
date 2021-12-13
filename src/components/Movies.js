21680406209ab0ac3b8a3a38255629efd8ba628d
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-13 16:48:26 +0900

    added firebase, removed counter, updadded images, added movies js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import styled from "styled-components";
+import { selectMovies } from '../features/movie/movieSlice';
+import { useSelector } from 'react-redux';
 
 const Movies = () => {
+  const movies = useSelector(selectMovies)
+
   return (
     <Container>
       <h4>Recommended for You</h4>
       <Content>
-        <Wrap>
-          <img src="/images/movies/barve.png" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/movies/blife.jpeg" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/movies/up.jpg" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/movies/wall-e.jpeg" />
-        </Wrap>
+        {movies &&
+          movies.map((movie) => (
+            <Wrap>
+              <img src={movie.cardImg} />
+            </Wrap>
+          ))
+        }
       </Content>
     </Container>
   );
@@ -27,37 +26,37 @@ export default Movies;
 
 const Container = styled.div`
 
-  `
+    `
 
 const Content = styled.div`
-  display: grid;
-  grid-gap: 25px;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
+    display: grid;
+    grid-gap: 25px;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
 
-  `
+    `
 
 const Wrap = styled.div`
-  border-radius: 10px;
-  overflow: hidden;
-  border: 3px solid rgba(249, 249, 249, 0.1);
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-  cursor: pointer;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-  margin-bottom: 40px;
-  max-height: 60vh;
+    border-radius: 10px;
+    overflow: hidden;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    margin-bottom: 40px;
+    max-height: 40vh;
+    max-width: 25vw;
 
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 
-  &:hover {
-    transform: scale(1.05);
-    border-color: rgba(249, 249, 249, 1);
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    &:hover {
+      transform: scale(1.05);
+      border-color: rgba(249, 249, 249, 1);
+      box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    }
 
-  }
-
-  `
+    `
