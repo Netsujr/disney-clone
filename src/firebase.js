91bad4dabe27aa565173bdeeabd936243b3c8f98
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-13 17:11:31 +0900

    fixed import problem, another problem arose :/
    TypeError: firebase_compat_app__WEBPACK_IMPORTED_MODULE_0__.default.storage is not a function

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // THIS FILE WAS CLONED NOT CREATED
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
