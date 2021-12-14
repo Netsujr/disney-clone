abec4a1c38d31ae398624ab969e534cca12d86ee
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-14 16:07:45 +0900

    Html change dlocation

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
+import App from '../src/App';
+import { store } from '../src/app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../src/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
