055360f8f7e4011028b7634116f58435e7e2397b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-14 16:17:37 +0900

    heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/App';
-import { store } from '../src/app/store';
+import App from './App';
+import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from '../src/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
